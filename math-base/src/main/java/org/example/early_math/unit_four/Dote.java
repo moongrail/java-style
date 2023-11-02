ab5f858dbf60dbf1e5950480e2105c2b3788501d
Moongrail <omgone666@yandex.ru>
2023-11-02 18:59:29 +0300

    feat: test

---
+++ math-base/src/main/java/org/example/early_math/unit_four/Dote.java
@@ -0,0 +1,44 @@
+package org.example.early_math.unit_four;
+
+import java.util.HashSet;
+
+public class Dote {
+//    public static boolean hasMirrorSymmetry(int[][] points) {
+//        HashSet<Integer> xSet = new HashSet<>();
+//
+//        for (int[] point : points) {
+//            xSet.add(point[0]);
+//        }
+//
+//        for (int[] point : points) {
+//            int mirrorX = -point[0];
+//            if (!xSet.contains(mirrorX)) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+
+    public static void main(String[] args) {
+        int[][] points = {{1, 2}, {-1, 2}, {3, 4}, {-3, 4}, {-4, 4}};
+
+        System.out.println("Points with mirror symmetry:");
+        for (int[] point : points) {
+            int mirrorX = -point[0];
+            int[] mirrorPoint = {mirrorX, point[1]};
+            if (containsPoint(points, mirrorPoint)) {
+                System.out.println("(" + point[0] + ", " + point[1] + ")");
+            }
+        }
+    }
+
+    private static boolean containsPoint(int[][] points, int[] targetPoint) {
+        for (int[] point : points) {
+            if (point[0] == targetPoint[0] && point[1] == targetPoint[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
